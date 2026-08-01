78dc637c43ea2ec2ed6145beca82788831615baf
vikramxsingh12 <7@felix-au.me>
2026-08-01 22:34:35 +0530

    Update queue.h - Step 7

--- queue.h
+++ queue.h
@@ -3,3 +3,9 @@ class Queue {
     std::vector<int> v;
 public:
     void push(int x) { v.push_back(x); }
+    int pop() {
+        int x = v.front();
+        v.erase(v.begin());
+        return x;
+    }
+};
